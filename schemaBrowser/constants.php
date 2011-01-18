f55a0d2878ea52170937b0ead6108980eef62037
becla <becla@git.lsstcorp.org>
2011-01-18 20:43:05 +0000

    adding PT1.2 schema to schema browser

--- schemaBrowser/constants.php
+++ schemaBrowser/constants.php
@@ -5,7 +5,7 @@ define ("DB_USER", "becla");
 define ("DB_PASS", "");
 
 define ("DB_NAME_PREFIX", "lsst_schema_browser_");
-$schemaVersions = array("DC3a", "DC3b", "PT1_1", "ImSim");
+$schemaVersions = array("DC3a", "DC3b", "PT1_1", "PT1_2", "ImSim");
 define ("DEFAULT_VERSION", "PT1_1");
 
 ?>
