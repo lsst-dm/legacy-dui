53e743d8363e4e42fd4c66be7d04ff31749eee01
Jacek Becla <becla@slac.stanford.edu>
2012-07-27 16:20:52 -0500

    enabled displaying S12_* schema

--- schemaBrowser/constants.php
+++ schemaBrowser/constants.php
@@ -3,7 +3,7 @@
 include_once("constants_auth.php");
 
 define ("DB_NAME_PREFIX", "lsst_schema_browser_");
-$schemaVersions = array("baseline", "DC3a", "PT1_1", "PT1_2", "ImSim");
+$schemaVersions = array("baseline", "DC3a", "PT1_1", "PT1_2", "ImSim", "S12_sdss", "S12_lsstsim");
 define ("DEFAULT_VERSION", "PT1_2");
 
 ?>
